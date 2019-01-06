42d4b4ac4683c35f1ab76dddfc68faa4e8245c31
Assoune Mike <assoune.mike@gmail.com>
2019-01-06 18:27:04 +0100

    c

---
+++ compare.js
@@ -0,0 +1,31 @@
+let one = []
+let two = []
+let valid = []
+
+const fs = require('fs');
+
+fs.readFile('napsterAccount.txt', 'utf8', function (err, data) {
+  if (err) return console.log(err);
+  one = data.split(',')
+
+  while (one.length) {
+    let account = one.shift()
+    if (one.indexOf(account) >= 0) {
+      console.log(account)
+    }
+    else {
+      valid.push(account)
+    }
+  }
+
+  fs.readFile('napsterAccountDel.txt', 'utf8', function (err, data) {
+    if (err) return console.log(err);
+    two = data.split(',')
+    valid = valid.filter(account => two.indexOf(account) === -1)
+
+    fs.writeFile('napsterAccount.txt', valid, function (err) {
+      if (err) return console.log(err);
+    });
+  })
+
+});
