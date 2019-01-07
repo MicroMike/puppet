92df54d3c36e040cf3649702c7f9436b0f2923b5
ASSOUNE Mike <massoune@nexity.fr>
2019-01-07 10:29:37 +0100

    c

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const request = require('ajax-request');
+
+process.setMaxListeners(Infinity)
+
 const check = process.env.CHECK
 let golbalAccountsLength = []
 let accounts = []
