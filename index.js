b84494628e9463c42a37225e982299e997a3880d
ASSOUNE Mike <massoune@nexity.fr>
2019-01-07 11:25:23 +0100

    c

--- index.js
+++ index.js
@@ -483,8 +483,7 @@ const main = async (restartAccount) => {
 
     if (player === 'napster') {
       const issue = await nightmare.evaluate(() => {
-        return document.querySelector('.unradio') && document.querySelector('.unradio').innerHTML ||
-          document.querySelector('.account-issue') && document.querySelector('.account-issue').innerHTML
+        return !!document.querySelector('.unradio') || !!document.querySelector('.account-issue')
       })
       console.log(issue)
       if (issue) { throw 'del' }
