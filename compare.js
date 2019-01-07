942850c5643ff065fad9042f083d4ca111af09ab
ASSOUNE Mike <massoune@nexity.fr>
2019-01-07 11:38:20 +0100

    c

--- compare.js
+++ compare.js
@@ -21,7 +21,13 @@ fs.readFile('napsterAccount.txt', 'utf8', function (err, data) {
   fs.readFile('napsterAccountDel.txt', 'utf8', function (err, data) {
     if (err) return console.log(err);
     two = data.split(',')
+    const length = valid.length
+
     valid = valid.filter(account => two.indexOf(account) === -1)
+
+    if (length !== valid.length) {
+      console.log(two)
+    }
 
     fs.writeFile('napsterAccount.txt', valid, function (err) {
       if (err) return console.log(err);
