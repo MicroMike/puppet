231604b6493820e87f703f35d091098ea601a56f
ASSOUNE Mike <massoune@nexity.fr>
2019-01-08 13:02:01 +0100

    c

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let accountsValid = []
 let over = false
 let countTimeout = 0
 const max = 20
-const pause = check ? 15 : 30
+const pause = check ? 10 : 30
 let errorPath = false
 
 const getTime = () => {
