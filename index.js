78979f3fc8c645a757c0eda2b6c254c87380f8d8
ASSOUNE Mike <massoune@nexity.fr>
2019-01-08 15:44:38 +0100

    c

--- index.js
+++ index.js
@@ -352,7 +352,7 @@ const main = async (restartAccount) => {
 
           if (!needCaptcha) { return resolve('click') }
 
-          const captcha = check && player === 'spotify' ? true : await anticaptcha(needCaptcha, keyCaptcha, true)
+          const captcha = process.env.CHECK ? true : await anticaptcha(needCaptcha, keyCaptcha, true)
           console.log('resolveCaptcha', 'done', login)
           if (captcha === 'error') { return resolve('error') }
 
