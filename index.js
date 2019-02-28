72a1d56ae5e865858ad3fb025b0bebc6e99329df
ASSOUNE Mike <MASSOUNE-EXT@nexity.fr>
2019-02-28 14:17:07 +0100

    c

--- index.js
+++ index.js
@@ -721,6 +721,14 @@ const main = async (restartAccount) => {
             if (!t1 || player !== 'napster') {
               fix = true
               if (!t1) {
+                if (player === 'tidal') {
+                  const delTidal = await page.evaluate(() => {
+                    return document.querySelector('.ReactModal__Overlay') && document.querySelector('.ReactModal__Overlay').innerText
+                  })
+                  if (typeof delTidal === 'string' && delTidal.match(/expired/)) {
+                    throw 'del'
+                  }
+                }
                 console.log(getTime(), ' no bar ' + t1, account)
                 await page.screenshot({ path: 'nobar_' + login + '_screenshot.png' });
               }
