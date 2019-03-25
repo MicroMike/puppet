909ebd39ed8ceb952dbbe01826a5ab8b5a28291b
Assoune Mike <assoune.mike@gmail.com>
2019-03-26 00:07:02 +0100

    c

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const request = require('ajax-request');
 var shell = require('shelljs');
 
-const check = process.env.CHECK || process.env.TYPE
+const check = process.env.CHECK === true || process.env.TYPE === true
 let accounts = []
 let accountsValid = 0
 let over = false
