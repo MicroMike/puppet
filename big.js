5ef75ec3a1a9668bf649cff9597aede5af1a7667
Assoune Mike <assoune.mike@gmail.com>
2019-03-26 09:05:56 +0100

    c

--- big.js
+++ big.js
@@ -2,4 +2,4 @@
 const shell = require('shelljs');
 process.setMaxListeners(Infinity)
 
-shell.exec('git reset --hard HEAD && git clean -f && git pull && find save/ -type f ! -iname "Cookies" -delete && npm run big')
+shell.exec('git reset --hard HEAD && git clean -f && git pull && npm run big')
