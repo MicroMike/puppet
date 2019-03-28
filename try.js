1ecc312a61a98ff2fb984a16619759cb5cd63b64
Assoune Mike <assoune.mike@gmail.com>
2019-03-28 23:16:00 +0100

    c

---
+++ try.js
@@ -0,0 +1,22 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({
+  electronPath: require('electron'),
+  // openDevTools: {
+  //   mode: 'detach'
+  // },
+  alwaysOnTop: false,
+  waitTimeout: 1000 * 60 * 3,
+  show: true,
+  width: 600,
+  height: 600,
+  typeInterval: 300,
+  webPreferences: {
+    partition: session,
+    webSecurity: false,
+    allowRunningInsecureContent: true,
+    plugins: true,
+    experimentalFeatures: true
+  }
+})
+
+nightmare.goto('https://spotify.com')
