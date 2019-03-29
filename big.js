29ae5038bb775ca73a5b65076fdc5a786908cfed
ASSOUNE Mike <massoune@nexity.fr>
2019-03-29 16:36:25 +0100

    c

--- big.js
+++ big.js
@@ -2,4 +2,5 @@
 const shell = require('shelljs');
 process.setMaxListeners(Infinity)
 
+shell.exec('cp runner.js node_modules/nightmare/lib/runner.js')
 shell.exec('git reset --hard HEAD && git clean -f && git pull && npm run big')
