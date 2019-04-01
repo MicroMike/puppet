c6768f506a1f0c9fcbe6b53b96c603974cb799c0
ASSOUNE Mike <massoune@nexity.fr>
2019-04-01 18:11:56 +0200

    c

--- big.js
+++
@@ -1,5 +0,0 @@
-
-const shell = require('shelljs');
-process.setMaxListeners(Infinity)
-
-shell.exec('git reset --hard HEAD && git clean -f && git pull && npm run big')
--- run.js
+++
@@ -1,8 +0,0 @@
-
-const shell = require('shelljs');
-
-shell.exec('git reset --hard HEAD && git clean -f && git pull')
-
-while (true) {
-  shell.exec('npm run all')
-}
