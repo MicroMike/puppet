f3786b57772a2c60bf09d7277e1283729c047c20
ASSOUNE Mike <massoune@nexity.fr>
2019-04-12 17:18:34 +0200

    c

--- run.js
+++ run.js
@@ -4,7 +4,7 @@ while (true) {
   shell.exec('killall chrome', { silent: true })
   // shell.exec('git clean -fd && git reset --hard HEAD')
   shell.exec('free -m && sync')
-  shell.exec('npm run rmcache', { silent: true })
+  shell.exec('sudo npm run rmcache', { silent: true })
   shell.exec('free -m')
   shell.exec('git pull')
   shell.exec('npm run all')
