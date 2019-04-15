d4f5bb084f6480c5572f01667b9865e20c24cd89
ASSOUNE Mike <massoune@nexity.fr>
2019-04-15 15:23:53 +0200

    c

--- index.js
+++ index.js
@@ -63,14 +63,15 @@ const main = async (account) => {
       // 5 = RETRY
       main(account)
     }
-    else if (code < 11) {
-      socket.emit('loop', { errorMsg, account })
-    }
 
     try {
-      const img = await image2base64(login + '_screenshot.png')
-      if (img && code > 1 && code < 11) {
-        socket.emit('screen', { img, log: account + ' => ' + errorMsg })
+      if (code < 11) {
+        socket.emit('loop', { errorMsg, account })
+
+        const img = await image2base64(login + '_screenshot.png')
+        if (img && code > 1) {
+          socket.emit('screen', { img, log: account + ' => ' + errorMsg })
+        }
       }
     }
     catch (e) { }
