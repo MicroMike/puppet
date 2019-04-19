61d7476cbfc16ead207dd3ea3b2140c265242df4
ASSOUNE Mike <massoune@nexity.fr>
2019-04-19 12:34:43 +0200

    c

--- run.js
+++ run.js
@@ -4,18 +4,18 @@ var socket = require('socket.io-client')('https://online-music.herokuapp.com');
 socket.on('activate', () => {
   socket.emit('startRun')
 
-  socket.on('stopped', () => {
+  socket.on('stop', () => {
     shell.exec('killall chrome', { silent: true })
   })
 
-  socket.on('started', () => {
+  socket.on('start', () => {
     shell.exec('npm run rm', { silent: true })
     shell.exec('git clean -fd && git reset --hard origin/master')
     shell.exec('git pull')
     shell.exec('npm run all')
   })
 
-  socket.on('reseted', () => {²
+  socket.on('reset', () => {
     shell.exec('killall chrome', { silent: true })
     shell.exec('npm run rm', { silent: true })
     shell.exec('git clean -fd && git reset --hard origin/master')
