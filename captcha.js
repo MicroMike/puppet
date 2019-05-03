c99b3d59523d978ef912362bc2788ca81ad6e017
ASSOUNE Mike <massoune@nexity.fr>
2019-05-03 14:35:02 +0200

    c

---
+++ captcha.js
@@ -0,0 +1,96 @@
+module.exports = async (page, websiteURL, websiteKey, username, login) => {
+  const request = require('ajax-request');
+  try {
+    const anticaptcha = (invisible = false) => {
+      return new Promise((resolve, reject) => {
+        request({
+          url: 'https://api.anti-captcha.com/createTask',
+          method: 'POST',
+          json: true,
+          data: {
+            clientKey: '1598b04fcee925998a78c2b75fd4dbd0',
+            task: {
+              type: 'NoCaptchaTaskProxyless',
+              websiteURL,
+              websiteKey,
+              invisible
+            }
+          }
+        }, function (err, res, response) {
+          if (!response || !response.taskId) {
+            console.log(response || 'no response')
+            resolve('error')
+            return;
+          }
+
+          const interval = setInterval(() => {
+            request({
+              url: 'https://api.anti-captcha.com/getTaskResult',
+              method: 'POST',
+              json: true,
+              data: {
+                clientKey: '1598b04fcee925998a78c2b75fd4dbd0',
+                taskId: response.taskId
+              }
+            }, function (err, res, response) {
+              try {
+                if (response && response.status !== 'processing') {
+                  clearInterval(interval)
+                  resolve(response.solution.gRecaptchaResponse)
+                }
+                else if (!response) {
+                  throw 'error'
+                }
+              }
+              catch (e) {
+                console.log(response || 'no response B')
+                clearInterval(interval)
+                resolve('error')
+                return;
+              }
+            });
+          }, 1000 * 30)
+        });
+      })
+    }
+
+    const resolveCaptcha = async () => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const captcha = await anticaptcha(true)
+          if (captcha === 'error') { return resolve('error') }
+
+          resolve(captcha)
+        }
+        catch (e) {
+          console.log(e)
+          resolve('error')
+        }
+      })
+    }
+
+    const captcha = await resolveCaptcha()
+
+    await page.rload()
+    await page.inst(username, login)
+
+    await page.evaluate((captcha) => {
+      setTimeout(() => {
+        let clients = window.___grecaptcha_cfg.clients[0]
+        Object.keys(clients).map(key => {
+          let client = clients[key]
+          Object.keys(client).map(k => {
+            let l = client[k]
+            l && l.callback && l.callback(captcha)
+          })
+        })
+      }, 5000);
+    }, captcha)
+
+    return true
+  }
+  catch (e) {
+    console.log(e)
+    return false
+  }
+}
---
+++ create.js
@@ -0,0 +1,40 @@
+process.setMaxListeners(0)
+
+var fs = require('fs');
+var shell = require('shelljs');
+const puppet = require('./puppet')
+const captcha = require('./captcha')
+
+const rand = (max, min) => {
+  return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
+}
+
+let url
+
+url = 'http://offer.tidal.com'
+keyCaptcha = '6Lf-ty8UAAAAAE5YTgJXsS3B-frcWP41G15z-Va2'
+
+const main = async () => {
+  const page = await puppet('', true)
+  const mailPage = await puppet('', true)
+
+  if (!page) { return }
+
+  await page.gotoUrl(url)
+  await page.click('body > div.content > div > div > div > div:nth-child(2) > div > button > div')
+
+  await mailPage.gotoUrl('https://temp-mail.org/fr/option/delete/')
+  const email = await mailPage.get('#mail', 'value')
+
+  await captcha(page, 'https://login.tidal.com/', keyCaptcha, 'input#email', email)
+
+  await page.inst('input#new-password', email)
+  await page.inst('input#password2', email)
+  await page.select('select#tbi-day', rand(25, 1))
+  await page.select('select#tbi-month', rand(12, 1))
+  await page.select('select#tbi-year', 1954 + rand(47))
+
+  await page.click('#registration-step-2 > button > div')
+}
+
+main()
