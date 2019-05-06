f342d998078c1ebea280a3d8dd02ad682625cbe6
ASSOUNE Mike <massoune@nexity.fr>
2019-05-06 15:57:17 +0200

    c

--- run.js
+++ run.js
@@ -2,8 +2,8 @@ var shell = require('shelljs');
 
 while (true) {
   shell.exec('killall chrome', { silent: true })
-  shell.exec('npm run rm', { silent: true })
-  shell.exec('git clean -fd && git reset --hard origin/master')
+  shell.exec('npm run rm && npm run clear', { silent: true })
+  shell.exec('git reset --hard origin/master')
   shell.exec('git pull')
   shell.exec('npm run all')
 }
