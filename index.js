5555b5606d610f04379ff5ca7b378da2fb77148f
ASSOUNE Mike <massoune@nexity.fr>
2019-05-20 12:06:21 +0200

    c

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let clientId
 
 const check = process.env.CHECK || process.env.TYPE
 let accountsValid = []
-const max = process.env.BIG ? 40 : 20
+const max = process.env.BIG ? 60 : 20
 let pause = false
 let first = true
 
