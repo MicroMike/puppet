2a7e207101492e5c59122b15359ab0c5f288d839
ASSOUNE Mike <massoune@nexity.fr>
2019-05-21 16:09:52 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -602,13 +602,13 @@ const fct = async () => {
           retry = false
           retryDom = false
           streamOn = false
-          socket.emit('retryOk')
+          // socket.emit('retryOk')
         }
 
         if (freeze > 3) {
+          freeze = 0
           await takeScreenshot('freeze')
           socket.emit('playerInfos', { account, time: t1, freeze: true })
-          // freeze = 0
 
           // if (!t1 && t1 !== 0) {
           //   throw 'nobar'
