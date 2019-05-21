b538315c394d919db7fcd0cb8e5c72e0f1007fb0
ASSOUNE Mike <massoune@nexity.fr>
2019-05-21 13:01:46 +0200

    c

--- puppet.js
+++ puppet.js
@@ -219,7 +219,7 @@ module.exports = async (userDataDir, noCache, cspot) => {
       // '--no-sandbox',
       // '--disable-setuid-sandbox',
       '--disable-translate',
-      '--user-agent=Mozilla/10.0 (Windows NT 10.0) AppleWebKit/538.36 (KHTML, like Gecko) Chrome/69.420 Safari/537.36'
+      // '--user-agent=Mozilla/10.0 (Windows NT 10.0) AppleWebKit/538.36 (KHTML, like Gecko) Chrome/69.420 Safari/537.36'
     ],
     defaultViewport: {
       width: 851,
