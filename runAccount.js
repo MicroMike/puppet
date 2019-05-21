44fd03779ef74ecc0cbf0fd8087729afa5ad37df
ASSOUNE Mike <massoune@nexity.fr>
2019-05-21 15:44:27 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -573,7 +573,7 @@ const fct = async () => {
         }
 
         t1 = await page.getTime(timeLine, callback)
-        // socket.emit('stayAlive', { login, player, time: t1 })
+        socket.emit('playerInfos', { account, time: t1 })
         await page.waitFor(1000 * 10)
         t2 = await page.getTime(timeLine, callback)
 
