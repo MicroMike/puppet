751bd765ea9d400cd1c5bc6cfc032763a00aada1
ASSOUNE Mike <massoune@nexity.fr>
2019-05-22 17:02:53 +0200

    c

--- runx.js
+++
@@ -1,9 +0,0 @@
-var shell = require('shelljs');
-
-while (true) {
-  shell.exec('killall chrome', { silent: true })
-  shell.exec('npm run rm && npm run clear', { silent: true })
-  shell.exec('git reset --hard origin/master')
-  shell.exec('git pull')
-  shell.exec('npm run xall')
-}
