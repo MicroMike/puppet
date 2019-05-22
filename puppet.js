87f370ef0bfd0194f4c27cc02213ac37e8e999ee
ASSOUNE Mike <massoune@nexity.fr>
2019-05-22 17:19:52 +0200

    c

--- puppet.js
+++ puppet.js
@@ -216,8 +216,8 @@ module.exports = async (userDataDir, noCache, cspot) => {
     userDataDir,
     headless: false,
     args: [
-      // '--no-sandbox',
-      // '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
       '--disable-translate',
       // '--user-agent=Mozilla/10.0 (Windows NT 10.0) AppleWebKit/538.36 (KHTML, like Gecko) Chrome/69.420 Safari/537.36'
     ],
