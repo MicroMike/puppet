997d37e3ba0990d17008c0db2ebf5b3658ff9cd8
ASSOUNE Mike <massoune@nexity.fr>
2019-05-23 10:44:21 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -571,8 +571,6 @@ const fct = async () => {
             throw 'used'
           }
         }
-        const timeget = await page.get(timeLine)
-        console.log(player, timeget)
 
         t1 = await page.getTime(timeLine, callback)
         await page.waitFor(1000 * 10)
