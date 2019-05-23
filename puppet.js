ae86da87901231f23aa8e692656512cd66e4180b
ASSOUNE Mike <massoune@nexity.fr>
2019-05-23 15:33:56 +0200

    c

--- puppet.js
+++ puppet.js
@@ -258,7 +258,6 @@ module.exports = async (userDataDir, noCache, cspot) => {
   let page = pages[0]
 
   page.bc = launch
-  page = addFcts(page)
 
   await page.evaluateOnNewDocument(() => {
     Object.defineProperty(navigator, 'webdriver', {
@@ -275,6 +274,8 @@ module.exports = async (userDataDir, noCache, cspot) => {
         interceptedRequest.continue();
     });
   }
+
+  page = addFcts(page)
 
   return page
 }
