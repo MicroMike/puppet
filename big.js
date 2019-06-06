31a2ef2ea5967f3af8bf75fb32654ed9f6230f6d
ASSOUNE Mike <massoune@nexity.fr>
2019-06-06 13:54:47 +0200

    c

--- big.js
+++ big.js
@@ -5,12 +5,5 @@ while (true) {
   shell.exec('npm run rm && npm run clear', { silent: true })
   shell.exec('git reset --hard origin/master')
   shell.exec('git pull')
-  shell.exec('npm run all', () => { })
-  shell.exec('npm run all', () => { })
-  shell.exec('npm run all', () => { })
-  shell.exec('npm run all', () => { })
-  shell.exec('npm run all', () => { })
-  shell.exec('npm run all', () => { })
-  shell.exec('npm run all', () => { })
-  shell.exec('npm run all')
+  shell.exec('npm run big')
 }
