83c5d21c9238a8ec7e839d96700d0cc0c6e02cb3
ASSOUNE Mike <massoune@nexity.fr>
2019-06-06 14:30:15 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -45,7 +45,7 @@ socket.on('activate', id => {
 request('https://online-accounts.herokuapp.com/albums', function (error, response, body) {
   const a = JSON.parse(body)
   albums = a[player]
-  console.log(a)
+  console.log(player, albums)
   fct()
 })
 
