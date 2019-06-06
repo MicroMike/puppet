7a58f64aa52b62eb8300b61f161c6348f785a57b
ASSOUNE Mike <massoune@nexity.fr>
2019-06-06 11:53:53 +0200

    c

--- index.js
+++ index.js
@@ -22,17 +22,17 @@ const getTime = () => {
 const main = async (account, isCheck) => {
   shell.exec('expressvpn disconnect', { silent: true })
 
-  if (!check && !updating) {
-    updating = true
-    shell.exec('npm run rm', { silent: true })
-    shell.exec('npm run clear', { silent: true })
-    shell.exec('git reset --hard origin/master', { silent: true })
-    shell.exec('git pull', { silent: true })
+  // if (!check && !updating) {
+  //   updating = true
+  //   shell.exec('npm run rm', { silent: true })
+  //   shell.exec('npm run clear', { silent: true })
+  //   shell.exec('git reset --hard origin/master', { silent: true })
+  //   shell.exec('git pull', { silent: true })
 
-    setTimeout(() => {
-      updating = false
-    }, 1000 * 60);
-  }
+  //   setTimeout(() => {
+  //     updating = false
+  //   }, 1000 * 60);
+  // }
 
   accountsValid.push(account)
   process.stdout.write(getTime() + " " + accountsValid.length + "\r");
