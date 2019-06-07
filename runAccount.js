c50b0eadb71a3d09ddaa348960524833a971a9a5
ASSOUNE Mike <massoune@nexity.fr>
2019-06-07 18:54:33 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -597,9 +597,9 @@ const fct = async () => {
           }
         }
 
-        const exist = await page.ext(timeLine)
-        !exist && await page.gotoUrl(album())
-        await waitForPlayBtn('failedLoop')
+        // const exist = await page.ext(timeLine)
+        // !exist && await page.gotoUrl(album())
+        // await waitForPlayBtn('failedLoop')
 
         t1 = await page.getTime(timeLine, callback)
         await page.waitFor(1000 * 10)
