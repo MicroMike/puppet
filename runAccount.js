c10480840c5e1c75afe51f4cd182946f7d17b155
ASSOUNE Mike <massoune@nexity.fr>
2019-06-13 13:10:51 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -326,7 +326,7 @@ const fct = async () => {
       loginBtn = '#login-button'
       loginError = '.alert.alert-warning'
 
-      playBtn = '.tracklist-play-pause.tracklist-middle-align'
+      playBtn = '.main-view-container button'
       repeatBtn = '[class*="spoticon-repeat"]'
       repeatBtnOk = '.spoticon-repeat-16.control-button--active'
       shuffleBtn = '.spoticon-shuffle-16:not(.control-button--active)'
