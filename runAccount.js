e3e79735e89486f3339328a61c9e2465cbf1bfc8
ASSOUNE Mike <massoune@nexity.fr>
2019-06-13 16:25:17 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -584,7 +584,7 @@ const fct = async () => {
         }
 
         t1 = await page.getTime(timeLine, callback)
-        await page.waitFor(1000 * 10)
+        await page.waitFor(1000 * 5)
         t2 = await page.getTime(timeLine, callback)
 
         let matchTime = Number(t1)
