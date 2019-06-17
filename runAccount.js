ab98fd958933d7e292f33b067e135ac1168123d2
ASSOUNE Mike <massoune@nexity.fr>
2019-06-17 11:40:09 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -572,7 +572,7 @@ const fct = async () => {
     let exitLoop = false
 
     let countPlays = 0
-    let changePlay = 10 + rand(5)
+    let changePlay = 10 + rand(20)
 
     const loop = async () => {
       if (player === 'tidal') {
