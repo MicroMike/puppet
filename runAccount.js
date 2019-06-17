3f6c8f3367730259df36f7419dddad00e70e02b3
ASSOUNE Mike <massoune@nexity.fr>
2019-06-17 17:55:37 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -241,7 +241,7 @@ const fct = async () => {
     code = e === 'del' ? 4 : code
     code = e === 'tidalError' ? 6 : code
     code = e === 'used' ? 7 : code
-    
+
     // code = e === 'retry' ? 5 : code
     // code = e === 'crashed' ? 6 : code
     // code = e === 'error' ? 7 : code
@@ -250,7 +250,7 @@ const fct = async () => {
     // code = e === 'nobar' ? 10 : code
     // code = e === 'check' ? 12 : code
 
-    if (code === 1 || code === 6) {
+    if (code === 1) {
       socket.emit('retryOk')
     }
 
