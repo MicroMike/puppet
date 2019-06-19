4d7e79c1081aa64e0b5870ec1b196ec57e4e2476
ASSOUNE Mike <massoune@nexity.fr>
2019-06-19 12:00:10 +0200

    c

--- create.js
+++ create.js
@@ -32,8 +32,8 @@ else {
 
 let count
 const main = async () => {
-  const page = await puppet('', true, true)
-  const mailPage = await page.np()
+  const mailPage = await puppet('', true, true)
+  const page = await page.np()
 
   if (!page) { return }
 
