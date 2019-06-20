215d7dcf3e6af766af8ebbc9cc9e32409a8bb6f6
ASSOUNE Mike <massoune@nexity.fr>
2019-06-20 15:17:03 +0200

    c

--- big.js
+++ big.js
@@ -1,16 +1,24 @@
 var shell = require('shelljs');
 
 const arg = process.argv[2]
-
-for (let i = 0; i < 13; i++) {
-  shell.exec('node index ' + arg, () => { })
-}
-
-const inter = setInterval(() => {
+const pull = () => {
   shell.exec('npm run rm && npm run clear', { silent: true })
   shell.exec('git reset --hard origin/master', { silent: true })
   shell.exec('git pull', { silent: true })
-}, 1000 * 60 * 2)
+}
+
+pull()
+const inter = setInterval(pull, 1000 * 60 * 2)
+
+let out = 0
+for (let i = 0; i < 13; i++) {
+  shell.exec('node index ' + arg, () => {
+    if (++out === 5) {
+      clearInterval(inter)
+      process.exit()
+    }
+  })
+}
 
 process.on('SIGINT', () => {
   clearInterval(inter)
