187f3bc209127953249644581d6e64ad6cac1343
ASSOUNE Mike <massoune@nexity.fr>
2019-07-11 14:22:14 +0200

    c

--- create.js
+++ create.js
@@ -75,7 +75,7 @@ const main = async () => {
 
     await page.waitFor(2000 + rand(2000))
 
-    await page.jClk('button:enabled', true)
+    await page.jClk('button.btn-success:enabled', true)
 
     await page.waitFor(2000 + rand(2000))
 
