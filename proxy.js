94b1081aee7a7d3e0d92bf87c7532b8b3e87a278
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-07-29 10:54:41 +0200

    c

---
+++ proxy.js
@@ -0,0 +1,6 @@
+var shell = require('shelljs');
+const arg = process.argv[2].split('.')
+const ip = arg[0] + '.' + arg[1] + '.' + arg[2] + '.'
+
+
+shell.exec(`/sbin/ip route del ${ip}0/24 via ${ip}1 dev ens18 && /sbin/ip route add ${ip}0/24 dev ens18 proto kernel scope link src ${ip + arg[3]} && expressvpn connect nl`)
