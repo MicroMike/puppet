e783501db04b4cad2789a1e04ee41baa664c92e4
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-07-30 15:33:32 +0200

    c

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const main = async () => {
   // process.stdout.write(`${getTime()} max: ${accountsValid >= max} ${accountsValid} \r`)
   console.log(`${nb} ${getTime()} max: ${accountsValid >= max} ${accountsValid}`)
 
-  let cmd = 'PORT=' + (port++) + 'CLIENTID=' + arg + ' TIME=' + time + ' node runAccount'
+  let cmd = 'PORT=' + (port++) + ' CLIENTID=' + arg + ' TIME=' + time + ' node runAccount'
   cmd = check ? 'CHECK=true ' + cmd : cmd
   cmd = first ? 'FIRST=true ' + cmd : cmd
   first = false
