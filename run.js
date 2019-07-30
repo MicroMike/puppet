535537c7a74cf166f2d47d53cda81b0cb71854de
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-07-30 14:08:57 +0200

    c

--- run.js
+++ run.js
@@ -14,7 +14,7 @@ pull()
 const inter = setInterval(pull, 1000 * 60 * 2)
 
 let out = 0
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 25; i++) {
   shell.exec('node index ' + arg, () => {
     console.log('OUUUUUUUUUUUUUUUUUUUUT')
     if (++out === 5) {
