ea69a2d7f4b87204ea8d997e6b9cc79da595c2f8
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-01 11:33:18 +0200

    c

--- new.js
+++ new.js
@@ -1,8 +1,33 @@
+process.setMaxListeners(0)
+
+const arg = process.argv[2]
+const nb = process.argv[3]
+
 var shell = require('shelljs');
 
 const arg = process.argv[2]
 
 shell.exec('killall chrome', { silent: true })
+
+const check = process.env.CHECK || process.env.TYPE
+let accountsValid = 0
+const max = process.env.BIG ? 63 : 1
+let pause = false
+let first = true
+let updating
+let timeout
+let time
+
+const rand = (max, min) => {
+  return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
+}
+
+const getTime = () => {
+  const date = new Date
+  const hour = date.getUTCHours() + 1
+  const minute = date.getUTCMinutes() < 10 ? '0' + date.getUTCMinutes() : date.getUTCMinutes()
+  return hour + 'H' + minute
+}
 
 const pull = () => {
   shell.exec('npm run rm && npm run clear', { silent: true })
@@ -13,20 +38,31 @@ const pull = () => {
 pull()
 const inter = setInterval(pull, 1000 * 60 * 2)
 
-let out = 0
-let up = 60
-let count = 0
-
 const run = async (i) => {
-  count++
-  shell.exec('node index ' + arg + ' ' + i, code => {
-    count--
-    console.log(count)
-    if (code === 10 && count === 0) {
-      clearInterval(inter)
+  try {
+    shell.exec('expressvpn disconnect', { silent: true })
+  }
+  catch (e) { }
+
+  accountsValid++
+  // process.stdout.write(`${getTime()} max: ${accountsValid >= max} ${accountsValid} \r`)
+  // console.log(`${nb} ${getTime()} max: ${accountsValid >= max} ${accountsValid}`)
+
+  let cmd = 'CLIENTID=' + arg + ' TIME=' + time + ' node runAccount'
+  cmd = check ? 'CHECK=true ' + cmd : cmd
+  cmd = first ? 'FIRST=true ' + cmd : cmd
+  first = false
+
+  shell.exec(cmd, async (code, b, c) => {
+    accountsValid--
+    console.log(`${nb} code: ${code}`, b, c)
+    // process.stdout.write(`${getTime()} max: ${accountsValid >= max} ${accountsValid} \r`)
+
+    if (code === 100) {
+      console.log('exit')
       process.exit()
     }
-    else if (code !== 10) {
+    else {
       run(i)
     }
   })
