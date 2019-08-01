cac7d95eb108b4c3a51fa58a304cf1a452ad5b08
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-01 17:40:34 +0200

    c

--- n.js
+++ n.js
@@ -3,6 +3,8 @@ var shell = require('shelljs');
 while (true) {
   shell.exec('xvfb-run -a node new ' + process.argv[2])
 
+  console.log('RESTART')
+
   shell.exec('npm run rm && npm run clear', { silent: true })
   shell.exec('git reset --hard origin/master', { silent: true })
   shell.exec('git pull', { silent: true })
