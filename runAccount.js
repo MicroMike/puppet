8c98be233cf7193141aa92c48375baa7af4b6ed5
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-06 16:09:33 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -614,7 +614,12 @@ const fct = async () => {
       await page.wfs('.album-tracks .options-button.icon-options')
       socket.emit('playerInfos', { account: player + ':' + login, streamId, time: 'ADDALBUMS', other: true })
       await page.evaluate(() => {
-        document.querySelectorAll('.album-tracks .options-button.icon-options').forEach(t => { t.click(); document.querySelector('.add-to-favorites').style['display'] !== 'none' && document.querySelector('.add-to-favorites').click() })
+        document.querySelectorAll('.album-tracks .options-button.icon-options').forEach(t => {
+          t.click()
+          setTimeout(() => {
+            document.querySelector('.add-to-favorites').style['display'] !== 'none' && document.querySelector('.add-to-favorites').click()
+          }, 300);
+        })
       })
       socket.emit('playerInfos', { account: player + ':' + login, streamId, time: 'PLAY', other: true })
       await page.clk('.thin-nav-button[title="Favorites"] a')
