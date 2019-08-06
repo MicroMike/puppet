4fd6d5345dc7a454be8e2b274e655d072ce1b8b9
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-06 11:44:33 +0200

    c

--- runAccount.js
+++ runAccount.js
@@ -611,7 +611,7 @@ const fct = async () => {
 
     if (player === 'napster') {
       socket.emit('playerInfos', { account: player + ':' + login, streamId, time: 'ADDALBUMS', other: true })
-      albums.forEach(a => {
+      albums.forEach(async a => {
         await page.gotoUrl(a)
         await page.evaluate(() => {
           document.querySelectorAll('.album-tracks .options-button.icon-options').forEach(t => { t.click(); document.querySelector('.add-to-favorites').style['display'] !== 'none' && document.querySelector('.add-to-favorites').click() })
