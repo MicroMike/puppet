12c7d35c6ac9648e0a014c61fca6e91b72a59126
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-20 11:50:22 +0200

    c

--- n2.js
+++ n2.js
@@ -14,8 +14,8 @@ const rand = (max, min) => {
 const pull = () => {
   try {
     const b = shell.exec('git fetch && git status', { silent: true })
-    console.log(b)
-    if (!/up to date/.test(b)) {
+    console.log(b.match(/up to date/g), b[0])
+    if (!b.match(/up to date/g)) {
       shell.exec('npm run rm && npm run clear', { silent: true })
       shell.exec('git reset --hard origin/master', { silent: true })
       shell.exec('git pull', { silent: true })
