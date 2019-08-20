3a47524b681a51cc7b82ad4efdbb9f37598940e6
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-20 12:02:33 +0200

    c

--- n3.js
+++ n3.js
@@ -7,19 +7,6 @@ const check = process.env.CHECK
 
 shell.exec('killall chrome', { silent: true })
 
-const rand = (max, min) => {
-  return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
-}
-
-const pull = () => {
-  try {
-    shell.exec('npm run rm && npm run clear', { silent: true })
-    shell.exec('git reset --hard origin/master', { silent: true })
-    shell.exec('git pull', { silent: true })
-  }
-  catch (e) { }
-}
-
 let out = false
 process.on('SIGINT', () => {
   out = true
@@ -31,20 +18,28 @@ try {
 }
 catch (e) { }
 
-const main = async () => {
+const main = async (wait = false) => {
   if (out) { return }
 
-  pull()
+  try {
+    const b = shell.exec('git fetch && git status', { silent: true })
+    if (!b.match(/up to date/g)) {
+      shell.exec('npm run rm && npm run clear', { silent: true })
+      shell.exec('git reset --hard origin/master', { silent: true })
+      shell.exec('git pull', { silent: true })
+    }
+  }
+  catch (e) { }
 
-  let cmd = 'CLIENTID=' + arg + ' TIME=' + Date.now() + ' node runAccount'
+  let cmd = 'WAIT=' + wait + ' CLIENTID=' + arg + ' TIME=' + Date.now() + ' node runAccount'
   cmd = check ? 'CHECK=true ' + cmd : cmd
 
   shell.exec(cmd, async (code, b, c) => {
-    console.log(`code: ${code}`, b, c)
+    // console.log(`code: ${code}`, b, c)
     main()
   })
 }
 
-for (let i = 0; i < 200; i++) {
-  main()
+for (let i = 0; i < 30; i++) {
+  main(true)
 }
