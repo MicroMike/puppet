b77c3d33868a98a637d101909fa98eaf19d7b998
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-27 11:17:35 +0200

    c

---
+++ night.js
@@ -0,0 +1,26 @@
+const Nightmare = require('nightmare')
+
+const page = Nightmare({
+	electronPath: require('electron'),
+	// openDevTools: {
+	//   mode: 'detach'
+	// },
+	alwaysOnTop: false,
+	waitTimeout: 1000 * 60,
+	show: true,
+	width: 851,
+	height: 450,
+	typeInterval: 300,
+	webPreferences: {
+		partition: persist,
+		webSecurity: false,
+		allowRunningInsecureContent: true,
+		plugins: true,
+		images: false,
+		experimentalFeatures: true
+	}
+})
+
+const main = async () => {
+	await page.goto('https://micro-napster.herokuapp.com')
+}
