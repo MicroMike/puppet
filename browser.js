749ef7df93cb6dd49f6aadb66754ff91a4f24ab9
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-28 11:13:11 +0200

    c

--- browser.js
+++ browser.js
@@ -73,7 +73,7 @@ const main = async () => {
       try {
         const inbox = (shell.exec('yogo_linux_amd64 inbox show ' + mail + ' 1', { silent: true })).stdout
 
-        code = isCode && String(inbox).split('suivant')[1].split('Ne partagez')[0]
+        code = isCode && String(inbox).split('suivant \:')[1].split('Ne partagez')[0]
 
         console.log('CODE: ' + code)
 
