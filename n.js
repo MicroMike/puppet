a914bc5aa7978d6a5a9185b477d81479e1b0445a
Assoune Mike <massoune-ext@nexity.fr>
2019-09-16 13:51:16 +0200

    c

--- n.js
+++ n.js
@@ -15,29 +15,33 @@ process.on('SIGINT', () => {
   process.exit()
 })
 
-for (let i = 0; i < thread; i++) {
-  while (true) {
-    if (close) { break }
-    console.log('----- START ' + i + ' ----- ')
+const fct = async () => {
+  for (let i = 0; i < thread; i++) {
+    while (true) {
+      if (close) { break }
+      console.log('----- START ' + i + ' ----- ')
 
-    shell.exec('killall chrome', { silent: true })
+      shell.exec('killall chrome', { silent: true })
 
-    const socket = require('socket.io-client')('https://online-music.herokuapp.com', { transports: ['websocket'] });
-    sockets[i] = socket
+      const socket = require('socket.io-client')('https://online-music.herokuapp.com', { transports: ['websocket'] });
+      sockets[i] = socket
 
-    try {
-      const b = shell.exec('git fetch && git status', { silent: true })
-      if (!b.match(/up to date/g)) {
-        shell.exec('npm run rm && npm run clear', { silent: true })
-        shell.exec('git reset --hard origin/master', { silent: true })
-        shell.exec('git pull', { silent: true })
+      try {
+        const b = shell.exec('git fetch && git status', { silent: true })
+        if (!b.match(/up to date/g)) {
+          shell.exec('npm run rm && npm run clear', { silent: true })
+          shell.exec('git reset --hard origin/master', { silent: true })
+          shell.exec('git pull', { silent: true })
+        }
       }
-    }
-    catch (e) { }
+      catch (e) { }
 
-    const start = require('./new')
-    await start(socket, arg, nb, i)
-    socket.disconnect()
-    sockets[i] = null
+      const start = require('./new')
+      await start(socket, arg, nb, i)
+      socket.disconnect()
+      sockets[i] = null
+    }
   }
 }
+
+fct()
