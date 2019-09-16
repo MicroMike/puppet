3fa25609acef8eaf4f641c0eef4b7e6cf8bd146a
Assoune Mike <massoune-ext@nexity.fr>
2019-09-16 13:45:10 +0200

    c

--- n.js
+++ n.js
@@ -1,21 +1,43 @@
 process.setMaxListeners(Infinity)
 
-var shell = require('shelljs');
+const shell = require('shelljs');
+const sockets = []
 
 const arg = process.argv[2]
 const nb = process.argv[3]
+const thread = process.argv[4]
+let close = false
 
-while (true) {
-  console.log('----- START -----')
-  try {
-    const b = shell.exec('git fetch && git status', { silent: true })
-    if (!b.match(/up to date/g)) {
-      shell.exec('npm run rm && npm run clear', { silent: true })
-      shell.exec('git reset --hard origin/master', { silent: true })
-      shell.exec('git pull', { silent: true })
+process.on('SIGINT', () => {
+  close = true
+  console.log('----- END -----')
+  sockets.forEach(s => s && s.disconnect())
+  process.exit()
+})
+
+for (let i = 0; i < thread; i++) {
+  while (true) {
+    if (close) { break }
+    console.log('----- START ' + i + ' ----- ')
+
+    shell.exec('killall chrome', { silent: true })
+
+    const socket = require('socket.io-client')('https://online-music.herokuapp.com', { transports: ['websocket'] });
+    sockets[i] = socket
+
+    try {
+      const b = shell.exec('git fetch && git status', { silent: true })
+      if (!b.match(/up to date/g)) {
+        shell.exec('npm run rm && npm run clear', { silent: true })
+        shell.exec('git reset --hard origin/master', { silent: true })
+        shell.exec('git pull', { silent: true })
+      }
     }
-  }
-  catch (e) { }
+    catch (e) { }
 
-  shell.exec('xvfb-run -a node --max-old-space-size=12288 new ' + arg + ' ' + nb)
+    const start = require('./new')
+    await start(socket, arg, nb, i)
+    socket.disconnect()
+    sockets[i] = null
+  }
 }
--- new.js
+++ new.js
@@ -1,94 +1,86 @@
-process.setMaxListeners(Infinity)
+module.exports = async (socket, arg, nb, thread) => {
+  return new Promise(async (r) => {
+    const puppet = require('./puppet')
+    const shell = require('shelljs');
 
-const puppet = require('./puppet')
-const shell = require('shelljs');
-const socket = require('socket.io-client')('https://online-music.herokuapp.com', { transports: ['websocket'] });
+    shell.exec('killall chrome', { silent: true })
 
-shell.exec('killall chrome', { silent: true })
-
-try {
-  shell.exec('expressvpn disconnect', { silent: true })
-}
-catch (e) { }
-
-process.on('SIGINT', () => {
-  socket.emit('disconnect')
-  console.log('----- END -----')
-  process.exit()
-})
-
-socket.on('Cdisconnect', () => {
-  socket.emit('disconnect')
-  console.log('----- END -----')
-  process.exit()
-})
-
-socket.on('killall', () => {
-  socket.emit('disconnect')
-  console.log('----- END -----')
-  shell.exec('killall node')
-})
-
-//Assign the event handler to an event:
-
-//Fire the 'scream' event:
-
-const arg = process.argv[2]
-const nb = process.argv[3]
-
-let parentId
-
-const inter = () => {
-  if (socket.connected) {
-    socket.emit('ping')
-    setTimeout(() => {
-      inter()
-    }, 1000 * 60);
-  }
-}
-
-socket.on('activate', () => {
-  console.log('activate', 'connected:' + !!parentId)
-  socket.emit('parent', { parentId: arg, connected: parentId, env: process.env, max: nb })
-  if (!parentId) { parentId = arg }
-  inter()
-})
-
-socket.on('run', async ({ runnerAccount, streamId }) => {
-  try {
-    const b = shell.exec('git fetch && git status', { silent: true })
-    if (!b.match(/up to date/g)) {
-      console.log('----- PULL -----')
-      shell.exec('npm run rm && npm run clear', { silent: true })
-      shell.exec('git reset --hard origin/master', { silent: true })
-      shell.exec('git pull', { silent: true })
+    try {
+      shell.exec('expressvpn disconnect', { silent: true })
     }
-    shell.exec('npm run buff', { silent: true })
-  }
-  catch (e) { }
+    catch (e) { }
 
-  const accountInfo = runnerAccount.split(':')
-  let player = accountInfo[0]
-  let login = accountInfo[1]
+    socket.on('Cdisconnect', () => {
+      socket.disconnect()
+      console.log('----- END ' + thread + ' -----')
+      r(true)
+    })
 
-  console.log('account', runnerAccount, player)
+    socket.on('killall', () => {
+      socket.disconnect()
+      console.log('----- END ' + thread + ' -----')
+      r(true)
+    })
 
-  if (process.env.CHECK) {
-    shell.exec('rm -Rf save/' + player + '_' + login, { silent: true })
-  }
+    //Assign the event handler to an event:
 
-  socket.emit('wait', parentId)
+    //Fire the 'scream' event:
 
-  const page = await puppet('save/' + player + '_' + login, player.match(/napster/))
+    let parentId
 
-  if (!page) {
-    console.log('no page')
-    socket.emit('stopWait', parentId)
-  }
-  else {
-    const runAccount = require('./runAccount');
-    const client = await runAccount(page, parentId, streamId, process.env, runnerAccount)
-    socket.emit('stopWait', parentId)
-    client.disconnect()
-  }
-})
+    const inter = () => {
+      if (socket.connected) {
+        socket.emit('ping')
+        setTimeout(() => {
+          inter()
+        }, 1000 * 60);
+      }
+    }
+
+    socket.on('activate', () => {
+      console.log('activate', 'connected:' + !!parentId)
+      socket.emit('parent', { parentId: arg, connected: parentId, env: process.env, max: nb })
+      if (!parentId) { parentId = arg + thread }
+      inter()
+    })
+
+    socket.on('run', async ({ runnerAccount, streamId }) => {
+      try {
+        const b = shell.exec('git fetch && git status', { silent: true })
+        if (!b.match(/up to date/g)) {
+          console.log('----- PULL -----')
+          shell.exec('npm run rm && npm run clear', { silent: true })
+          shell.exec('git reset --hard origin/master', { silent: true })
+          shell.exec('git pull', { silent: true })
+        }
+        shell.exec('npm run buff', { silent: true })
+      }
+      catch (e) { }
+
+      const accountInfo = runnerAccount.split(':')
+      let player = accountInfo[0]
+      let login = accountInfo[1]
+
+      console.log('account', runnerAccount, player)
+
+      if (process.env.CHECK) {
+        shell.exec('rm -Rf save/' + player + '_' + login, { silent: true })
+      }
+
+      socket.emit('wait', parentId)
+
+      const page = await puppet('save/' + player + '_' + login, player.match(/napster/))
+
+      if (!page) {
+        console.log('no page')
+        socket.emit('stopWait', parentId)
+      }
+      else {
+        const runAccount = require('./runAccount');
+        const client = await runAccount(page, parentId, streamId, process.env, runnerAccount)
+        socket.emit('stopWait', parentId)
+        client.disconnect()
+      }
+    })
+  })
+}
