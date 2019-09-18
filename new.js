2aa2df48d3ebf4fb505565c0a59d8cb1a02e2296
Assoune Mike <massoune-ext@nexity.fr>
2019-09-18 11:32:06 +0200

    c

--- new.js
+++ new.js
@@ -5,10 +5,11 @@ const shell = require('shelljs');
 const socket = require('socket.io-client')('https://online-music.herokuapp.com', { transports: ['websocket'] });
 
 const arg = process.argv[2]
-const nb = process.argv[3]
-const thread = process.argv[4]
+const nb = Number(process.argv[3])
+const thread = Number(process.argv[4])
 
 let CS = {}
+let close = false
 
 shell.exec('killall chrome', { silent: true })
 
@@ -17,12 +18,20 @@ try {
 }
 catch (e) { }
 
-const exit = () => {
-  console.log('----- END ' + thread + ' -----')
-  Object.values(CS).forEach(s => s.disconnect())
-  CS = {}
-  socket.disconnect()
-  process.exit()
+const exit = (noExit = false) => {
+  !noExit && console.log('----- END ' + thread + ' -----')
+
+  Object.values(CS).forEach(({ clientSocket, page, streamId }) => {
+    clientSocket.disconnect()
+    page.cls(true)
+    delete CS[streamId]
+  })
+
+  if (!noExit) {
+    close = true
+    socket.disconnect()
+    process.exit()
+  }
 }
 
 process.on('SIGINT', () => {
@@ -31,6 +40,10 @@ process.on('SIGINT', () => {
 
 socket.on('Cdisconnect', () => {
   exit()
+})
+
+socket.on('CdisconnectU', () => {
+  exit(true)
 })
 
 socket.on('killall', () => {
@@ -44,12 +57,13 @@ socket.on('killall', () => {
 let parentId
 
 const inter = () => {
-  if (socket.connected) {
+  if (Object.values(CS).length < max) {
     socket.emit('ping')
-    setTimeout(() => {
-      inter()
-    }, 1000 * 60);
   }
+
+  setTimeout(() => {
+    if (!close) { inter() }
+  }, 1000 * 30);
 }
 
 socket.on('activate', () => {
@@ -91,7 +105,7 @@ socket.on('run', async ({ runnerAccount, streamId }) => {
   }
   else {
     const clientSocket = require('socket.io-client')('https://online-music.herokuapp.com', { transports: ['websocket'] });
-    CS[streamId] = clientSocket
+    CS[streamId] = { clientSocket, page, streamId }
 
     const runAccount = require('./runAccount');
     await runAccount(clientSocket, page, parentId, streamId, process.env, runnerAccount)
