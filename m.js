2c1a07bf6d4cf2b8962b00b04c619e841d2e1807
Assoune Mike <massoune-ext@nexity.fr>
2020-01-29 17:54:45 +0100

    c

---
+++ m.js
@@ -0,0 +1,63 @@
+process.setMaxListeners(Infinity)
+
+const shell = require('shelljs');
+// const sockets = []
+
+const arg = process.argv[2]
+const nb = Number(process.argv[3]) || 1
+
+let close = false
+
+const rand = (max, min) => {
+  return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
+}
+
+process.on('SIGINT', () => {
+  close = true
+  console.log('----- END -----')
+  shell.exec('killall -9 node', { silent: true })
+  process.exit()
+})
+
+const fct = async (i = 1) => {
+  if (close) { return }
+
+  try {
+    const b = shell.exec('git fetch && git status', { silent: true })
+    if (!b.match(/up to date/g)) {
+      console.log('----- PULL ' + thread + ' -----')
+      shell.exec('npm run rm && npm run clear', { silent: true })
+      shell.exec('git reset --hard origin/master', { silent: true })
+      shell.exec('git pull', { silent: true })
+    }
+  }
+  catch (e) { }
+
+  console.log('----- START ' + i + ' ----- ')
+
+  const ram = shell.exec('free -m |awk \'{ print $2 }\' | awk \'NR == 2\'', { silent: true }).stdout.trim()
+  shell.exec('xvfb-run -a node --max-old-space-size=' + ram + ' neww ' + arg, () => {
+    try {
+      const b = shell.exec('git fetch && git status', { silent: true })
+      if (!b.match(/up to date/g)) {
+        console.log('----- PULL ' + thread + ' -----')
+        shell.exec('npm run rm && npm run clear', { silent: true })
+        shell.exec('git reset --hard origin/master', { silent: true })
+        shell.exec('git pull', { silent: true })
+      }
+    }
+    catch (e) { }
+
+    fct(i)
+  })
+
+  setTimeout(() => {
+    if (i <= nb) {
+      fct(++i)
+    }
+  }, rand(1000 * 60 * 2));
+}
+
+shell.exec('killall -9 chrome', { silent: true })
+
+fct()
---
+++ neww.js
@@ -0,0 +1,123 @@
+process.setMaxListeners(Infinity)
+
+const puppet = require('./puppet')
+const shell = require('shelljs');
+const request = require('ajax-request');
+
+const arg = process.argv[2]
+
+let CS = {}
+let timeout
+let close = false
+let account
+
+shell.exec('killall -9 chrome', { silent: true })
+
+try {
+  shell.exec('expressvpn disconnect', { silent: true })
+}
+catch (e) { }
+
+const exit = (noExit = false) => {
+  request('https://online-music.herokuapp.com/noUseAccount?' + account)
+  process.exit()
+
+  // !noExit && console.log('----- END ' + thread + ' -----')
+
+  // Object.values(CS).forEach(({ clientSocket, page, streamId }) => {
+  //   clientSocket.disconnect()
+  //   page.cls(true)
+  //   delete CS[streamId]
+  // })
+
+  // clearTimeout(timeout)
+
+  // if (!noExit) {
+  //   close = true
+  //   socket.disconnect()
+  //   process.exit()
+  // }
+}
+
+process.on('SIGINT', () => {
+  exit()
+})
+
+socket.on('Cdisconnect', () => {
+  exit()
+})
+
+socket.on('CdisconnectU', () => {
+  exit(true)
+})
+
+socket.on('killall', () => {
+  exit()
+})
+
+const rand = (max, min) => {
+  return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
+}
+
+let back
+let parentId
+
+const streamId = rand(10000) + '-' + rand(10000) + '-' + rand(10000) + '-' + rand(10000)
+
+const createCallback = (error, response, runnerAccount) => {
+  account = runnerAccount;
+
+  if (!account) { return console.log('no account') }
+
+  const accountInfo = account.split(':')
+  let player = accountInfo[0]
+  let login = accountInfo[1]
+
+  if (process.env.CHECK) {
+    shell.exec('rm -Rf save/' + player + '_' + login, { silent: true })
+  }
+
+  let page
+
+  try {
+    page = await puppet('save/' + player + '_' + login, player.match(/napster/))
+  }
+  catch (e) { page = false }
+
+  if (!page) {
+    console.log(thread + ' no page')
+  }
+  else {
+    const clientSocket = require('socket.io-client')('https://online-music.herokuapp.com', { transports: ['websocket'] });
+
+    const runAccount = require('./runAccount');
+    await runAccount(clientSocket, page, arg, streamId, process.env, account)
+
+    exit()
+  }
+}
+
+const main = () => {
+  try {
+    const b = shell.exec('git fetch && git status', { silent: true })
+    if (!b.match(/up to date/g)) {
+      console.log('----- PULL ' + thread + ' -----')
+      shell.exec('npm run rm && npm run clear', { silent: true })
+      shell.exec('git reset --hard origin/master', { silent: true })
+      shell.exec('git pull', { silent: true })
+    }
+    shell.exec('npm run buff', { silent: true })
+  }
+  catch (e) { }
+
+  request('https://online-music.herokuapp.com/useAccount', createCallback)
+}
+
+main()
+
+socket.on('activate', async (socketId) => {
+  back = !!parentId
+  parentId = arg
+
+  socket.emit('client', { parentId, streamId, account, back })
+})
