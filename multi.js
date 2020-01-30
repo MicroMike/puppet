7709fcc09f10aad7d779ea49cd445213f0c0b126
Mike <assoune.mike@gmail.com>
2020-01-30 19:22:23 +0100

    c

--- multi.js
+++ multi.js
@@ -6,9 +6,12 @@ const request = require('ajax-request');
 
 const arg = process.argv[2]
 const nb = Number(process.argv[3]) || 1
-const thread = Number(process.argv[4]) || 1
 
 let close = false
+
+const rand = (max, min) => {
+  return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
+}
 
 process.on('SIGINT', () => {
   close = true
@@ -37,7 +40,7 @@ const fct = async (i = 1) => {
   console.log('----- START ' + i + ' ----- ')
 
   const ram = shell.exec('free -m |awk \'{ print $2 }\' | awk \'NR == 2\'', { silent: true }).stdout.trim()
-  shell.exec('xvfb-run -a node --max-old-space-size=' + ram + ' multi ' + arg+thread + ' ' + nb, () => {
+  shell.exec('node neww ' + arg + ' ' + nb, () => {
     try {
       const b = shell.exec('git fetch && git status', { silent: true })
       if (!b.match(/up to date/g)) {
@@ -54,9 +57,10 @@ const fct = async (i = 1) => {
   })
 }
 
-shell.exec('killall -9 chrome', { silent: true })
-
-for (let i = 1; i <= thread; i++) {
+for (let i = 1; i <= nb; i++) {
   if (close) { break }
-  fct(i)
+  setTimeout(() => {
+    if (close) { return }
+    fct(i)
+  }, 1000 * 3 * i);
 }
