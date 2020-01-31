2c1335c4401b817eeb86a475da0cb8aa819d6b84
Assoune Mike <massoune-ext@nexity.fr>
2020-01-31 13:19:18 +0100

    c

--- m.js
+++ m.js
@@ -38,17 +38,6 @@ const fct = async (i = 1) => {
 
   const ram = shell.exec('free -m |awk \'{ print $2 }\' | awk \'NR == 2\'', { silent: true }).stdout.trim()
   shell.exec('xvfb-run -a node --max-old-space-size=' + ram + ' multi ' + current + ' ' + nb, () => {
-    try {
-      const b = shell.exec('git fetch && git status', { silent: true })
-      if (!b.match(/up to date/g)) {
-        console.log('----- PULL ' + current + ' -----')
-        shell.exec('npm run rm && npm run clear', { silent: true })
-        shell.exec('git reset --hard origin/master', { silent: true })
-        shell.exec('git pull', { silent: true })
-      }
-    }
-    catch (e) { }
-
     if (close) { return }
     fct(i)
   })
--- multi.js
+++ multi.js
@@ -24,6 +24,17 @@ const fct = async (i = 1) => {
 
   const ram = shell.exec('free -m |awk \'{ print $2 }\' | awk \'NR == 2\'', { silent: true }).stdout.trim()
   shell.exec('node neww ' + arg + ' ' + nb, () => {
+    try {
+      const b = shell.exec('git fetch && git status', { silent: true })
+      if (!b.match(/up to date/g)) {
+        console.log('----- PULL ' + arg + ' -----')
+        shell.exec('npm run rm && npm run clear', { silent: true })
+        shell.exec('git reset --hard origin/master', { silent: true })
+        shell.exec('git pull', { silent: true })
+      }
+    }
+    catch (e) { }
+
     if (close) { return }
     fct(i)
   })
