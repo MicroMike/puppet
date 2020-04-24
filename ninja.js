bad0d8fc653200db36130be70bdb9ee0895421af
Mike <assoune.mike@gmail.com>
2020-04-24 16:08:13 +0200

    c

--- ninja.js
+++ ninja.js
@@ -5,13 +5,40 @@ process.on('SIGINT', () => {
     exit = true
 })
 
+const rand = (max, min) => {
+    return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
+}
+
+const urls = [
+    "tripadvisor.com",
+    "qq.com",
+    "fastcompany.com",
+    "amazon.com",
+    "shop-pro.jp",
+    "flavors.me",
+    "npr.org",
+    "arstechnica.com",
+    "pinterest.com",
+    "bloglines.com",
+    "deliciousdays.com",
+    "qq.com",
+    "feedburner.com",
+    "github.com",
+    "nsw.gov.au",
+    "cornell.edu",
+    "columbia.edu",
+    "paypal.com",
+    "google.es",
+    "deliciousdays.com"
+]
+
 const main = async () => {
 
     const browser = await puppeteer.launch({ headless: false });
     const pages = await browser.pages()
     const page = pages[0]
 
-    await page.goto('http://gestyy.com/w91Mg8');
+    await page.goto('http://sh.st/st/445f592bec4c2e1aaa4e0c4ad0c22836/' + urls[rand(urls.length)]);
 
     try {
         await page.waitForSelector('.skip-btn.show', { timeout: 1000 * 10 })
