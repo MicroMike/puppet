d73508fa87d238ba21824db48fe1b5e7a99e3232
Mike <assoune.mike@gmail.com>
2021-06-22 11:44:20 +0200

    new index

--- index.js
+++ index.js
@@ -1,74 +1,135 @@
-process.setMaxListeners(0)
-const arg = process.argv[2]
-const nb = process.argv[3]
+const CDP = require('chrome-remote-interface');
+const fs = require('fs');
+const shell = require('shelljs');
 
-var shell = require('shelljs');
-// var fs = require('fs');
-// var socket = require('socket.io-client')('https://online-music.herokuapp.com');
+const port = process.argv[2]
+// const port = 1233
+let N, P, R;
 
-const check = process.env.CHECK || process.env.TYPE
-let accountsValid = 0
-const max = process.env.BIG ? 63 : 1
-let pause = false
-let first = true
-let updating
-let timeout
-let time
+const connect = () => new Promise((resolve, reject) => {
+	const loop = async () => {
+		const ls = await shell.exec('chrome-remote-interface list --port=' + port, { silent: false })
+		console.log(/Error/.test(ls.stderr))
+		if (/Error/.test(ls.stderr)) {
+			loop()
+			console.log(ls.stderr)
+		} else {
+			resolve(ls.stdout)
+		}
+	}
 
-const rand = (max, min) => {
-  return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
+	loop()
+})
+
+const expression = `var keyboardEvent = document.createEvent('KeyboardEvent');
+var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+
+keyboardEvent[initMethod](
+  'keypress', // event type: keydown, keyup, keypress
+  true, // bubbles
+  true, // cancelable
+  window.body, // view: should be window
+  false, // ctrlKey
+  false, // altKey
+  false, // shiftKey
+  false, // metaKey
+  0, // keyCode: unsigned long - the virtual key code, else 0
+  'a', // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
+);
+document.dispatchEvent(keyboardEvent);`
+
+const waitForSelector = (selector) => new Promise((res, rej) => {
+	const loop = async () => {
+		const el = await R.evaluate({ expression: 'document.querySelector(\'' + selector + '\')' })
+		console.log(el)
+		if (!el.result.objectId) {
+			loop()
+		} else {
+			console.log(selector + ' found')
+			res('ok')
+		}
+	}
+
+	loop()
+
+	setTimeout(() => {
+		res('notOk')
+	}, 30 * 1000);
+})
+
+const click = (selector) => new Promise(async (res, rej) => {
+	const wfs = await waitForSelector(selector)
+	if (wfs !== 'ok') { return }
+
+	setTimeout(async () => {
+		await R.evaluate({ expression: 'document.querySelectorAll(\'' + selector + '\')[0].click()' })
+		res('ok')
+	}, 3000);
+})
+
+const takeScreenshot = async (filename = 'sc.png') => {
+	const { data } = await Page.captureScreenshot();
+	fs.writeFileSync(filename, Buffer.from(data, 'base64'));
 }
 
-const getTime = () => {
-  const date = new Date
-  const hour = date.getUTCHours() + 1
-  const minute = date.getUTCMinutes() < 10 ? '0' + date.getUTCMinutes() : date.getUTCMinutes()
-  return hour + 'H' + minute
+async function example() {
+	// await shell.exec('"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome" --no-first-run --user-data-dir="tidal' + port + '" --remote-debugging-port=' + port, { async: true })
+	await shell.exec('google-chrome-stable --no-sandbox --disable-gpu --no-first-run --user-data-dir="tidal' + port + '" --remote-debugging-port=' + port, { async: true })
+
+	await connect()
+
+	const options = {
+		host: '127.0.0.1',
+		port
+	}
+
+	CDP(options, async (client) => {
+		console.log('Connected!');
+
+		try {
+			// connect to endpoint
+			client = await CDP(options);
+
+			// extract domains
+			const { Network, Page, Runtime } = client;
+			N = Network;
+			P = Page;
+			R = Runtime;
+
+			// setup handlers
+			Network.requestWillBeSent((params) => {
+				// console.log(params.request.url);
+			});
+			// enable events then start!
+			await Network.enable();
+			await Page.enable();
+			await Page.navigate({ url: 'https://listen.tidal.com/album/88716570/track/88716580' });
+			await Page.loadEventFired();
+
+			// await shell.exec('osascript space.scpt')
+
+			const wfs = await waitForSelector('[datatest="no-user--signup"]')
+
+			if (wfs === 'ok') {
+				await click('[datatest="no-user--signup"]')
+				await waitForSelector('#email')
+				await takeScreenshot()
+			} else {
+				await click('[data-test="shuffle-all"]')
+			}
+		} catch (err) {
+			console.error(err);
+		} finally {
+			if (client) {
+				// await client.close();
+			}
+		}
+
+		// client.close();
+	}).on('error', (err) => {
+		console.error(err);
+	});
+
 }
 
-const main = async () => {
-  try {
-    shell.exec('expressvpn disconnect', { silent: true })
-    if (check) {
-      // shell.exec('expressvpn connect nl', { silent: true })
-    }
-  }
-  catch (e) { }
-
-  accountsValid++
-  // process.stdout.write(`${getTime()} max: ${accountsValid >= max} ${accountsValid} \r`)
-  // console.log(`${nb} ${getTime()} max: ${accountsValid >= max} ${accountsValid}`)
-
-  let cmd = 'CLIENTID=' + arg + ' TIME=' + time + ' node runAccount'
-  cmd = check ? 'CHECK=true ' + cmd : cmd
-  cmd = first ? 'FIRST=true ' + cmd : cmd
-  first = false
-
-  shell.exec(cmd, async (code, b, c) => {
-    accountsValid--
-    console.log(`${nb} code: ${code}`)
-    // process.stdout.write(`${getTime()} max: ${accountsValid >= max} ${accountsValid} \r`)
-
-    if (code === 100) {
-      console.log('exit')
-      process.exit(100)
-    }
-
-    main()
-  })
-}
-
-main()
-
-// timeout = setInterval(() => {
-//   time = time || Date.now()
-
-//   // process.stdout.write(`${getTime()} max: ${accountsValid >= max} ${accountsValid} \r`)
-//   if (check || accountsValid < max) { main() }
-// }, check ? 1000 * 60 : rand(1000 * 60 * 5));
-
-process.on('SIGINT', () => {
-  clearInterval(timeout)
-  console.log('exit')
-  process.exit(100)
-});
+example();
