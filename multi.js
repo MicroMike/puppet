8dbc3457e0cbc0a2bb4c3f79728506042f5fa368
Assoune Mike <assoune.mike@gmail.com>
2021-09-12 17:21:41 +0200

    c

--- multi.js
+++ multi.js
@@ -27,13 +27,13 @@ const fct = async (i = 1) => {
   // const ram = shell.exec('free -m |awk \'{ print $2 }\' | awk \'NR == 2\'', { silent: true }).stdout.trim()
   shell.exec('node neww ' + arg + ' ' + nb, (a, b, c) => {
     try {
-      // const b = shell.exec('git fetch && git status', { silent: true })
-      // if (!b.match(/up to date/g)) {
-      //   console.log('----- PULL ' + arg + ' -----')
-      //   shell.exec('npm run rm && npm run clear', { silent: true })
-      //   shell.exec('git reset --hard origin/master', { silent: true })
-      //   shell.exec('git pull', { silent: true })
-      // }
+      const b = shell.exec('git fetch && git status', { silent: true })
+      if (!b.match(/up to date/g)) {
+        console.log('----- PULL ' + arg + ' -----')
+        shell.exec('npm run rm && npm run clear', { silent: true })
+        shell.exec('git reset --hard origin/master', { silent: true })
+        shell.exec('git pull', { silent: true })
+      }
     }
     catch (e) { }
 
