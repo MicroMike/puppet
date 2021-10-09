8ebf58f1b799fb562316647f99831fdee1902aac
Mike <assoune.mike@gmail.com>
2021-10-09 22:42:50 +0200

    c

--- neww.js
+++ neww.js
@@ -24,8 +24,8 @@ let login
 const copyBack = () => {
 	try {
 		shell.exec('node keepCookie', { silent: false })
-		shell.exec('scp -r /root/puppet/' + login + ' root@216.158.239.199:/root/puppet/', { silent: false })
-		shell.exec('rm -rf /root/puppet/' + login, { silent: false })
+		shell.exec('scp -r /root/puppet/puppet/' + login + ' root@216.158.239.199:/root/puppet/', { silent: false })
+		shell.exec('rm -rf /root/puppet/puppet/' + login, { silent: false })
 	} catch (e) {
 		console.log(e)
 	}
@@ -114,7 +114,7 @@ clientSocket.on('mRun', async () => {
 	}
 
 	try {
-		shell.exec('scp -r root@216.158.239.199:/root/puppet/' + login + ' /root/puppet', { silent: false })
+		shell.exec('scp -r root@216.158.239.199:/root/puppet/puppet/' + login + ' /root/puppet', { silent: false })
 	} catch (e) {
 		console.log(e)
 	}
