e449166b2b352b11c0a7a01ddf2a52ca18dc0f97
Mike <assoune.mike@gmail.com>
2021-10-10 11:27:04 +0200

    c

--- multi.js
+++ multi.js
@@ -10,41 +10,43 @@ const nb = Number(process.argv[3]) || 1
 let close = false
 
 const exit = () => {
-  close = true
-  shell.exec('killall -9 chrome', { silent: true })
-  shell.exec('taskkill /F /IM chrome.exe', { silent: true })
-  console.log('----- END ' + arg + ' -----')
-  process.exit()
+	close = true
+	shell.exec('killall -9 chrome', { silent: true })
+	shell.exec('taskkill /F /IM chrome.exe', { silent: true })
+	console.log('----- END ' + arg + ' -----')
+	process.exit()
 }
 
 process.on('SIGINT', () => {
-  exit()
+	exit()
 })
 
 const fct = async (i = 1) => {
-  if (close) { return }
+	if (close) { return }
 
-  // const ram = shell.exec('free -m |awk \'{ print $2 }\' | awk \'NR == 2\'', { silent: true }).stdout.trim()
-  shell.exec('node neww ' + arg + ' ' + nb, (a, b, c) => {
-    try {
-      const b = shell.exec('git fetch && git status', { silent: true })
-      if (!b.match(/up to date/g)) {
-        console.log('----- PULL ' + arg + ' -----')
-        shell.exec('npm run rm && npm run clear', { silent: true })
-        shell.exec('git reset --hard origin/master', { silent: true })
-        shell.exec('git pull', { silent: true })
-      }
-    }
-    catch (e) { }
+	shell.exec('rm -rf /root/puppet/puppet/*' + login, { silent: false })
 
-    if (b === '1') { exit() }
+	// const ram = shell.exec('free -m |awk \'{ print $2 }\' | awk \'NR == 2\'', { silent: true }).stdout.trim()
+	shell.exec('node neww ' + arg + ' ' + nb, (a, b, c) => {
+		try {
+			const b = shell.exec('git fetch && git status', { silent: true })
+			if (!b.match(/up to date/g)) {
+				console.log('----- PULL ' + arg + ' -----')
+				shell.exec('npm run rm && npm run clear', { silent: true })
+				shell.exec('git reset --hard origin/master', { silent: true })
+				shell.exec('git pull', { silent: true })
+			}
+		}
+		catch (e) { }
 
-    if (close) { return }
-    fct(i)
-  })
+		if (b === '1') { exit() }
+
+		if (close) { return }
+		fct(i)
+	})
 }
 
 for (let i = 1; i <= nb; i++) {
-  if (close) { break }
-  fct(i)
+	if (close) { break }
+	fct(i)
 }
