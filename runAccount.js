0e6bbe70a26109724f0a6f57c9ac4ceea329cd4b
Mike <assoune.mike@gmail.com>
2021-11-11 12:59:42 +0100

    c

--- runAccount.js
+++ runAccount.js
@@ -842,17 +842,11 @@ module.exports = async (socket, page, parentId, streamId, check, account) => {
 				}
 			}
 
-			// if (check) {
-			// 	request('http://216.158.239.199:3000' + '/checkOk?' + account, async (error, response, body) => {
-			// 		// startCheck()
-			// 		// shell.exec('git add save/' + player + '_' + login + ' && git commit -m "add account" && git push')
-			// 		mailPage && await mailPage.cls(true)
-			// 		await page.cls(true)
-
-			// 		catchFct('check')
-			// 	})
-			// 	return
-			// }
+			if (check) {
+				await page.cls(true)
+				catchFct('check')
+				returnƒ
+			}
 
 			// ***************************************************************************************************************************************************************
 			// *************************************************************************** LOOP ******************************************************************************
