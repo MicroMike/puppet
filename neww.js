4ff1ffffbdc7ff7aacc146f3b1fde977c7961118
Mike <assoune.mike@gmail.com>
2021-11-14 12:20:50 +0100

    c

--- neww.js
+++ neww.js
@@ -164,12 +164,12 @@ clientSocket.on('mRun', async () => {
 	// 	await runAccount(clientSocket, page, parentId, streamId, arg === 'check', account)
 	// }
 
-	const runAccount = checkAccount ? require('./index') : require('./runAccount');
+	const runAccount = check || checkAccount ? require('./index') : require('./runAccount');
 	// const runTidal = require('./index');
 	if (check) {
 		console.log(account);
 	}
-	page = !checkAccount && await puppet(varPath + player + login)
+	page = !check && !checkAccount && await puppet(varPath + player + login)
 	await runAccount(clientSocket, page, parentId, streamId, arg === 'check', account)
 
 	exit()
