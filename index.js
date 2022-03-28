062b5ff098f826db54e82a692335b89e27183609
Mike <assoune.mike@gmail.com>
2022-03-28 10:05:39 +0200

    c

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = async (socket, page, parentId, streamId, check, account) => {
 			loginError: '.box-error',
 			email: '#email',
 			pass: '#password',
-			connectBtn: '[ui-test-id="check-user-continue-button"]',
+			connectBtn: '.btn-client-primary',
 			play: '[data-test="shuffle-all"]',
 			timeLine: '[data-test="current-time"]',
 			callback: a => (a.split(':').reduce((a, b) => a * 60 + Number(b))),
