fd83e9eecfc633681d5d3a0fdf438776b6383de2
Mike <assoune.mike@gmail.com>
2022-06-30 14:42:47 +0200

    c

--- index.js
+++ index.js
@@ -807,8 +807,8 @@ module.exports = async (socket, page, parentId, streamId, check, account) => {
 						isTidal && shell.exec('rm -rf /root/puppet/puppet/' + player + login + '/Default/Cache', { silent: true })
 
 						clearTimeout(timeout)
-						shell.exec('scp -r /root/puppet/puppet/' + player + login + ' root@216.158.239.199:/root/puppet/', { silent: true })
-						first && console.log('end save copy'.yellow, account)
+						shell.exec('scp -r /root/puppet/puppet/' + player + login + ' root@216.158.239.199:/root/puppet/', { async: true, silent: true })
+						// first && console.log('end save copy'.yellow, account)
 					} catch (e) {
 						console.log(e)
 					}
