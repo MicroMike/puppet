d91ac282d985bb6fa0904456c0acdeb8a189b0e8
Mike <assoune.mike@gmail.com>
2022-07-13 12:03:13 +0200

    c

--- launchtest.js
+++ launchtest.js
@@ -1,6 +1,11 @@
 const { spawn } = require("child_process");
 
-// await shell.exec('google-chrome-stable --no-sandbox --disable-gpu --disable-setuid-sandbox --no-first-run --disable-features=Translate --user-data-dir="puppet/' + player + login + '" --remote-debugging-port=' + port, { async: true, silent: true })
+const ls = spawn("google-chrome-stable", ['--no-sandbox', '--disable-gpu', '--disable-setuid-sandbox', '--no-first-run', '--disable-features=Translate']);
 
+ls.stdout.on("data", data => {
+	console.log(`stdout: ${data}`);
+});
+
+// await shell.exec('google-chrome-stable --no-sandbox --disable-gpu --disable-setuid-sandbox --no-first-run --disable-features=Translate --user-data-dir="puppet/' + player + login + '" --remote-debugging-port=' + port, { async: true, silent: true })
 
 spawn('google-chrome-stable');
