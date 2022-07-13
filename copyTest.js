d22686286e9ecac55331e708d6bf0d9b739d439b
Mike <assoune.mike@gmail.com>
2022-07-13 19:24:36 +0200

    c

--- copyTest.js
+++ copyTest.js
@@ -1,4 +1,4 @@
 const shell = require('shelljs');
 
-shell.exec('scp -r root@216.158.239.199:/root/puppet/tidalnawof36088@iistoria.com/Local Storage /root/puppet/puppet/ ', { async: !check, silent: true })
-shell.exec('scp -r root@216.158.239.199:/root/puppet/tidalnawof36088@iistoria.com/Session Storage /root/puppet/puppet/ ', { async: !check, silent: true })
+shell.exec('scp -r root@216.158.239.199:/root/puppet/tidalnawof36088@iistoria.com/Local Storage /root/puppet/puppet/ ', { silent: true })
+shell.exec('scp -r root@216.158.239.199:/root/puppet/tidalnawof36088@iistoria.com/Session Storage /root/puppet/puppet/ ', { silent: true })
