7ce422baaafc7401d4517fba5668f5f3ac8183ed
Mike <assoune.mike@gmail.com>
2022-07-13 12:03:27 +0200

    c

--- launchtest.js
+++ launchtest.js
@@ -8,4 +8,4 @@ ls.stdout.on("data", data => {
 
 // await shell.exec('google-chrome-stable --no-sandbox --disable-gpu --disable-setuid-sandbox --no-first-run --disable-features=Translate --user-data-dir="puppet/' + player + login + '" --remote-debugging-port=' + port, { async: true, silent: true })
 
-spawn('google-chrome-stable');
+// spawn('google-chrome-stable');
