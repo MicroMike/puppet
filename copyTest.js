6c6ddead77347b7d271bf71a068da612b2ed9341
Mike <assoune.mike@gmail.com>
2022-07-13 19:42:03 +0200

    c

--- copyTest.js
+++ copyTest.js
@@ -1,4 +1,4 @@
-const shell = require('shelljs');
+const { spawn } = require("child_process");
 
-shell.exec('scp -r root@216.158.239.199:"/root/puppet/tidalnawof36088@iistoria.com/Default/Local Storage" "/root/puppet/puppet/tidalnawof36088@iistoria.com/Default/Local Storage"')
-shell.exec('scp -r root@216.158.239.199:"/root/puppet/tidalnawof36088@iistoria.com/Default/Session Storage" "/root/puppet/puppet/tidalnawof36088@iistoria.com/Default/Session Storage"')
+spawn('scp -r root@216.158.239.199:"/root/puppet/tidalnawof36088@iistoria.com/Default/Local Storage" "/root/puppet/puppet/tidalnawof36088@iistoria.com/Default/Local Storage"')
+spawn('scp -r root@216.158.239.199:"/root/puppet/tidalnawof36088@iistoria.com/Default/Session Storage" "/root/puppet/puppet/tidalnawof36088@iistoria.com/Default/Session Storage"')
