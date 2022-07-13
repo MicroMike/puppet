453c6bb55883e94379b7e3d39673fe47c2a22142
Mike <assoune.mike@gmail.com>
2022-07-13 19:58:07 +0200

    c

--- copyTest.js
+++ copyTest.js
@@ -1,5 +1,5 @@
 const { exec } = require("child_process");
 
-exec('mkdir -p /root/puppet/puppet/tidalnawof36088@iistoria.com/Default')
-exec('scp -r root@216.158.239.199:"/root/puppet/tidalnawof36088@iistoria.com/Default/Session\\ Storage" /root/puppet/puppet/tidalnawof36088@iistoria.com/Default/')
-exec('scp -r root@216.158.239.199:"/root/puppet/tidalnawof36088@iistoria.com/Default/Local\\ Storage" /root/puppet/puppet/tidalnawof36088@iistoria.com/Default/');
+exec('mkdir -p /root/puppet/puppet/amazondirn.maks@protonmail.com/Default')
+exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Session\\ Storage" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/')
+exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Local\\ Storage" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
