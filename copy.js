5502ca04812bd83790befa5dbcf8ab494a03b37d
Mike <assoune.mike@gmail.com>
2022-07-13 23:13:11 +0200

    c

--- copy.js
+++ copy.js
@@ -25,14 +25,24 @@ const copyBack = (player, login) => new Promise((res, rej) => {
 	const folder = player + login
 
 	if (player === 'tidal') {
-		exec(`mkdir -p /root/puppet/${folder}/Default`)
+		// exec(`mkdir -p /root/puppet/${folder}/Default`)
 		exec(`scp -r /root/puppet/puppet/${folder}/Default/Session\\ Storage root@216.158.239.199:"/root/puppet/${folder}/Default/"`)
 		exec(`scp -r /root/puppet/puppet/${folder}/Default/Local\\ Storage root@216.158.239.199:"/root/puppet/${folder}/Default/"`)
 	}
 
 	if (player === 'amazon') {
-		exec(`mkdir -p /root/puppet/${folder}/Default`)
-		exec(`scp -r /root/puppet/puppet/${folder}/Default/Session\\ Storage root@216.158.239.199:"/root/puppet/${folder}/Default/"`)
+		// exec(`mkdir -p /root/puppet/${folder}/Default`)
+		exec(`scp -r /root/puppet/puppet/${folder}/Default/Session\\ Storage root@216.158.239.199:"/root/puppet/${folder}/Default/"`, (error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+				return;
+			}
+			console.log(`stdout: ${stdout}`);
+		})
 		exec(`scp -r /root/puppet/puppet/${folder}/Default/Local\\ Storage root@216.158.239.199:"/root/puppet/${folder}/Default/"`)
 		exec(`scp -r /root/puppet/puppet/${folder}/Default/Login\\ Data\\ For\\ Account root@216.158.239.199:"/root/puppet/${folder}/Default/"`)
 		exec(`scp -r /root/puppet/puppet/${folder}/Default/Login\\ Data root@216.158.239.199:"/root/puppet/${folder}/Default/"`)
