d642053f8c454949e9cb750f205716f448bcceef
Mike <assoune.mike@gmail.com>
2022-07-13 22:50:56 +0200

    c

--- copyTest.js
+++ copyTest.js
@@ -7,9 +7,9 @@ exec('scp -r root@216.158.239.199:"/root/puppet/tidalnawof36088@iistoria.com/Def
 exec('mkdir -p /root/puppet/puppet/amazondirn.maks@protonmail.com/Default')
 exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Session\\ Storage" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/')
 exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Local\\ Storage" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
-exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Login\\ Data\\ For\\ Account-journal" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
+// exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Login\\ Data\\ For\\ Account-journal" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
 exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Login\\ Data\\ For\\ Account" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
-exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Login\\ Data-journal" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
+// exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Login\\ Data-journal" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
 exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Login\\ Data" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
-exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Cookies-journal" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
+// exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Cookies-journal" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
 exec('scp -r root@216.158.239.199:"/root/puppet/amazondirn.maks@protonmail.com/Default/Cookies" /root/puppet/puppet/amazondirn.maks@protonmail.com/Default/');
