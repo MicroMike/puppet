74a7fc467fee371854ec254433261c4a14e3d100
Mike <assoune.mike@gmail.com>
2023-05-10 01:42:14 +0200

    c

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-cd "/root/puppet"
+cd "/root/mproject"
 rm google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-npm run google
+dpkg -i google-chrome-stable_current_amd64.deb
